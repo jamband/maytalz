76cf069f2908e53354c9dfa044251372e3565d60
Tomoki Morita <tmsongbooks215@gmail.com>
2020-01-15 17:44:33 +0900

    Add content on about page

--- pages/about.vue
+++ pages/about.vue
@@ -1,7 +1,13 @@
 <template>
   <div>
     <h1>About</h1>
-    about...
+    This is a application built on Nuxt.js + Contentful + Netlify.<br>
+    This was built for demonstration purpose.<br>
+    <br>
+    For more information:
+    <a href="https://github.com/jamband/nuxt-contentful-netlify-demo" rel="noopener" target="_blank">
+      GitHub: jamband/nuxt-contentful-netlify-demo
+    </a>
   </div>
 </template>
 
