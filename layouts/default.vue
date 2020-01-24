604d6ecd5f44f6f410d073c1459c751fc263cb37
Tomoki Morita <tmsongbooks215@gmail.com>
2020-01-24 21:51:45 +0900

    Change container max size and some fixes

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,7 +5,7 @@
         <b-navbar-brand to="/">{{ $app.name }}</b-navbar-brand>
         <b-navbar-toggle target="nav-collapse" />
         <b-collapse id="nav-collapse" is-nav>
-          <b-navbar-nav>
+          <b-navbar-nav class="ml-auto">
             <b-nav-item :to="{ name: 'contact' }">Contact</b-nav-item>
             <b-nav-item :to="{ name: 'about' }">About</b-nav-item>
           </b-navbar-nav>
