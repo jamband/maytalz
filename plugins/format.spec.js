c00cf8f46c04a02aebf205df3119e414e7886d6b
Tomoki Morita <tmsongbooks215@gmail.com>
2020-02-18 23:14:04 +0900

    Remove describe() in format.spec.js

--- plugins/format.spec.js
+++ plugins/format.spec.js
@@ -7,38 +7,34 @@ localVue.use(pluginFormat)
 
 const $format = localVue.prototype.$format
 
-describe('plugins: format.date()', () => {
-  test('2020-12-31 and 2021-1-1', () => {
-    expect($format.date(new Date('2020-12-31'))).toBe('2020.12.31')
-    expect($format.date(new Date('2020-1-1'))).toBe('2020.01.01')
-  })
+test('date: 2020-12-31 and 2021-1-1', () => {
+  expect($format.date(new Date('2020-12-31'))).toBe('2020.12.31')
+  expect($format.date(new Date('2020-1-1'))).toBe('2020.01.01')
 })
 
-describe('plugins: format.embedResponsive()', () => {
-  const src = {
-    YouTube: 'https://www.youtube.com/embed/foo',
-    SoundCloud: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/123'
-  }
+const src = {
+  YouTube: 'https://www.youtube.com/embed/foo',
+  SoundCloud: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/123'
+}
 
-  test('when including YouTube embed', () => {
-    expect($format.embedResponsive(`<iframe src="${src.YouTube}"></iframe>`))
-      .toBe(`<div class="embed-responsive embed-responsive-16by9"><iframe class="embed-responsive-item" src="${src.YouTube}"></iframe></div>`)
-  })
+test('embedResponsive: when including YouTube embed', () => {
+  expect($format.embedResponsive(`<iframe src="${src.YouTube}"></iframe>`))
+    .toBe(`<div class="embed-responsive embed-responsive-16by9"><iframe class="embed-responsive-item" src="${src.YouTube}"></iframe></div>`)
+})
 
-  test('when including SoundCloud embed', () => {
-    expect($format.embedResponsive(`<iframe src="${src.SoundCloud}"></iframe>`))
-      .toBe(`<iframe style="width: 100%; height: 120px;" src="${src.SoundCloud}"></iframe>`)
-  })
+test('embedResponsive: when including SoundCloud embed', () => {
+  expect($format.embedResponsive(`<iframe src="${src.SoundCloud}"></iframe>`))
+    .toBe(`<iframe style="width: 100%; height: 120px;" src="${src.SoundCloud}"></iframe>`)
+})
 
-  test('when including multiple YouTube embed', () => {
-    expect($format.embedResponsive(`<iframe src="${src.YouTube}"></iframe>\n`.repeat(2)))
-      .toBe(`<div class="embed-responsive embed-responsive-16by9"><iframe class="embed-responsive-item" src="${src.YouTube}"></iframe></div>\n` +
-        `<div class="embed-responsive embed-responsive-16by9"><iframe class="embed-responsive-item" src="${src.YouTube}"></iframe></div>\n`)
-  })
+test('embedResponsive: when including multiple YouTube embed', () => {
+  expect($format.embedResponsive(`<iframe src="${src.YouTube}"></iframe>\n`.repeat(2)))
+    .toBe(`<div class="embed-responsive embed-responsive-16by9"><iframe class="embed-responsive-item" src="${src.YouTube}"></iframe></div>\n` +
+      `<div class="embed-responsive embed-responsive-16by9"><iframe class="embed-responsive-item" src="${src.YouTube}"></iframe></div>\n`)
+})
 
-  test('when including YouTube and SoundCloud embed', () => {
-    expect($format.embedResponsive(`<iframe src="${src.YouTube}"></iframe>\n<iframe src="${src.SoundCloud}"></iframe>`))
-      .toBe(`<div class="embed-responsive embed-responsive-16by9"><iframe class="embed-responsive-item" src="${src.YouTube}"></iframe></div>\n` +
-        `<iframe style="width: 100%; height: 120px;" src="${src.SoundCloud}"></iframe>`)
-  })
+test('embedResponsive: when including YouTube and SoundCloud embed', () => {
+  expect($format.embedResponsive(`<iframe src="${src.YouTube}"></iframe>\n<iframe src="${src.SoundCloud}"></iframe>`))
+    .toBe(`<div class="embed-responsive embed-responsive-16by9"><iframe class="embed-responsive-item" src="${src.YouTube}"></iframe></div>\n` +
+      `<iframe style="width: 100%; height: 120px;" src="${src.SoundCloud}"></iframe>`)
 })
