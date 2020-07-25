5301a3cff0b5bac3ed1046384446d1a1752b3e01
Tomoki Morita <tmsongbooks215@gmail.com>
2020-07-25 18:17:29 +0900

    Fix font-family

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,7 +11,7 @@ export default {
       { hid: 'description', name: 'description', content: `${APP_NAME} is a simple blog` }
     ],
     link: [
-      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,600' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@400;600&display=swap' },
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
