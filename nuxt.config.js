25475df988f8fda7f0bcf19f1c59b9199d44ce98
Tomoki Morita <tmsongbooks215@gmail.com>
2021-05-17 09:29:07 +0900

    Add htmlAttrs in head section

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,6 +9,9 @@ export default {
   ],
   head: {
     titleTemplate: `%s - ${APP_NAME}`,
+    htmlAttrs: {
+      lang: 'en'
+    },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
