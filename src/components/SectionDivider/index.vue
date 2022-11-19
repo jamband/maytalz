fe27f9da703a32aa2f4bcddcc5653da3937cadd2
Tomoki Morita <tmsongbooks215@gmail.com>
2022-11-19 13:56:06 +0900

    darken section divider

--- src/components/SectionDivider/index.vue
+++ src/components/SectionDivider/index.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="flex items-center justify-center gap-1" role="separator">
-    <div class="h-1 w-1 rounded-[1px] bg-gray-400" />
-    <div class="h-3 w-1 rounded-[1px] bg-gray-400" />
-    <div class="h-6 w-1 rounded-[1px] bg-gray-600" />
-    <div class="h-4 w-1 rounded-[1px] bg-gray-500" />
-    <div class="h-7 w-1 rounded-[1px] bg-gray-400" />
+    <div class="h-1 w-1 rounded-[1px] bg-gray-500" />
     <div class="h-3 w-1 rounded-[1px] bg-gray-500" />
-    <div class="h-1 w-1 rounded-[1px] bg-gray-400" />
+    <div class="h-6 w-1 rounded-[1px] bg-gray-700" />
+    <div class="h-4 w-1 rounded-[1px] bg-gray-600" />
+    <div class="h-7 w-1 rounded-[1px] bg-gray-500" />
+    <div class="h-3 w-1 rounded-[1px] bg-gray-600" />
+    <div class="h-1 w-1 rounded-[1px] bg-gray-500" />
   </div>
 </template>
