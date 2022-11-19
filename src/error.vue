fb26ab2449c76939436ad8660149f296505ecd56
Tomoki Morita <tmsongbooks215@gmail.com>
2022-11-19 22:18:47 +0900

    improve not found page

---
+++ src/error.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+const error = useError();
+
+const isNotFound = Number(error.value?.statusCode || 500) === 404;
+const title = isNotFound ? "Not Found" : "An Error Occurred";
+const message = isNotFound ? "Page not found." : error.value?.message;
+</script>
+
+<template>
+  <div>
+    <NuxtLayout>
+      <TheHead :title="title" description="" />
+      <h1>{{ title }}</h1>
+      <p class="mb-16">{{ message }}</p>
+      <SectionDivider class="mb-16" />
+      <BackToHomeLink />
+    </NuxtLayout>
+  </div>
+</template>
--- src/pages/404.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div>
-    <h1>Not Found</h1>
-    <p class="mb-16">The page does not exist.</p>
-    <SectionDivider class="mb-10" />
-    <BackToHomeLink />
-  </div>
-</template>
