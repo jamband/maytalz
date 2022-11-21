fe13b68ade84de7bab51e552b10cdcc321ce2bac
Tomoki Morita <tmsongbooks215@gmail.com>
2022-11-21 19:25:54 +0900

    improve post links again

--- src/components/Post/Link/index.vue
+++ src/components/Post/Link/index.vue
@@ -8,12 +8,14 @@ const props = defineProps<{
 </script>
 
 <template>
-  <h2 class="group">
+  <h2>
     <NuxtLink
       :to="`/posts/${props.post.fields.slug}`"
-      class="peer text-gray-600 no-underline group-hover:text-orange-600 group-active:text-orange-600"
-      >{{ post.fields.title
-      }}<span
+      class="group text-gray-600 no-underline"
+      ><span class="group-hover:text-orange-600 group-active:text-orange-600">{{
+        post.fields.title
+      }}</span
+      ><span
         class="ml-2 align-top text-[1.25rem] text-gray-400 group-hover:text-orange-600 group-active:text-orange-600"
         >→</span
       >
