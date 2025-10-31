a6f52850783edfa29741a5d01e9d423a84935e0d
Tomoki Morita <tmsongbooks215@gmail.com>
2025-10-31 16:01:20 +0900

    improve eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,22 +3,23 @@ import prettier from "eslint-config-prettier";
 import vue from "eslint-plugin-vue";
 import { defineConfig, globalIgnores } from "eslint/config";
 import ts from "typescript-eslint";
+import vueParser from "vue-eslint-parser";
 
 export default defineConfig(
   [globalIgnores([".nuxt/", ".output/"])],
-  js.configs.recommended,
-  ts.configs.recommended,
-  ...vue.configs["flat/recommended"],
-  prettier,
   {
-    files: ["**/*.vue"],
+    files: ["**/*.{js,ts}", "app/**/*.vue"],
+    extends: [
+      js.configs.recommended,
+      ts.configs.recommended,
+      vue.configs["flat/recommended"],
+    ],
     languageOptions: {
+      parser: vueParser,
       parserOptions: {
         parser: ts.parser,
       },
     },
-  },
-  {
     rules: {
       "@typescript-eslint/consistent-type-imports": "error",
       "no-console": ["error", { allow: ["warn", "error"] }],
@@ -26,4 +27,5 @@ export default defineConfig(
       "vue/multi-word-component-names": "off",
     },
   },
+  prettier,
 );
